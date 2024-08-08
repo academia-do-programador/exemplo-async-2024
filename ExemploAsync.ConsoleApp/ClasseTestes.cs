5bfa42e0e9fb0993f60410ffab9cf3a94d769605
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-08-08 12:43:06 -0300

    Add project files.

---
+++ ExemploAsync.ConsoleApp/ClasseTestes.cs
@@ -0,0 +1,21 @@
+﻿namespace ExemploAsync.ConsoleApp;
+
+public class ClasseTestes
+{
+    public string ExecutarTimeoutEmSegundos(int segundos)
+    {
+        Thread.Sleep(TimeSpan.FromSeconds(segundos));
+
+        return $"Timeout de {segundos} segundo(s)";
+    }
+    
+    public Task<string> ExecutarTimeoutEmSegundosAsync(int segundos)
+    {
+        return Task.Run(() =>
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(segundos));
+
+            return $"Timeout de {segundos} segundo(s)";
+        });
+    }
+}
---
+++ ExemploAsync.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+
+namespace ExemploAsync.ConsoleApp;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        ClasseTestes classe = new ClasseTestes();
+
+        Stopwatch sw = new Stopwatch();
+        
+        sw.Start();
+        
+        var umSegundo = classe.ExecutarTimeoutEmSegundosAsync(1);
+        var doisSegundos = classe.ExecutarTimeoutEmSegundosAsync(2);
+        var tresSegundos = classe.ExecutarTimeoutEmSegundosAsync(3);
+        
+        sw.Stop();
+        
+        Console.WriteLine(await umSegundo);
+        Console.WriteLine(await doisSegundos);
+        Console.WriteLine(await tresSegundos);
+
+        var tempoDecorrido = sw.Elapsed;
+
+        Console.WriteLine($"\nO tempo decorrido nos processos foi de: {tempoDecorrido} segundo(s)");
+        
+        Console.ReadLine();
+    } 
+}
